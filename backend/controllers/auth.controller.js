0e1eff6652dd595e2e532f0aaa453a4149cd74cf
prajwal <spiderbasnet140@gmail.com>
2025-05-30 18:52:32 +0545

    backend

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req,res) =>{
+    res.send("signup User")
+}
+
+export const login = (req,res) =>{
+    console.log("login User");
+}
+
+export const logout = (req,res) =>{
+    console.log("Logout User")
+}
