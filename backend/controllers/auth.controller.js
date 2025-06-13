8dd71ba7c0c2319131aad6bc13f43ff7a2abdc0a
prajwal <spiderbasnet140@gmail.com>
2025-06-13 09:41:43 +0545

    some files

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async(req,res) =>{
     try {
@@ -14,25 +16,36 @@ export const signup = async(req,res) =>{
             return res.status(400).json({error:"User already exists"})
         }
         // Hash Password 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
         // https://avatar-placeholder.iran.liara.run/
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
         const newUser = new User({
             fullName,
             username,
-            password,
+            password:hashedPassword,
             gender,
             profilePic: gender === "male"?boyProfilePic : girlProfilePic
         })
-        await newUser.save();
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username:newUser.username,
-            profilePic:newUser.profilePic
-        })
-
+   
+        if(newUser){
+            // Generate JWT token 
+            generateTokenAndSetCookie(newUser._id,res)
+            await newUser.save();
+        
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username:newUser.username,
+                profilePic:newUser.profilePic
+            })
+    
+        }else{
+            res.status(400).json({error:"invalid data"})
+        }
     } catch (error) {
         console.log("Error in singup controller " , error.message);
         res.status(500).json({
@@ -42,7 +55,14 @@ export const signup = async(req,res) =>{
 }
 
 export const login = async(req,res) =>{
+    
+    try{
+
+    }catch(error){
+        
+    }
     res.send("login User");
+
 }
 
 export const logout = async(req,res) =>{
