9c221330f348f8a4fe25838a210bde21efcab0ae
Nishant Niroula <niroulanishant910@gmail.com>
2025-06-28 22:09:13 +0545

    added message sending functionality

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -16,4 +16,7 @@ const messageSchema = new mongoose.Schema({
         type : String,
         required:true
     }
-},{timestamps:true})
+},
+{timestamps:true})
+const Message= mongoose.model("Message",messageSchema);
+export default Message;
