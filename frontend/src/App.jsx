557f8b7b6a689d40f5740a03741764bb31d8eb83
prajwal <spiderbasnet140@gmail.com>
2025-06-30 00:45:28 +0545

    done lg and sg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        <Login></Login>
+       <Signup></Signup>
       </div>
     </>
   )
