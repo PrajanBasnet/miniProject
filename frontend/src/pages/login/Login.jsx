9a5e5fae3f8d66593eac264c6cd0e9622324883e
prajwal <spiderbasnet140@gmail.com>
2025-07-19 13:48:53 +0545

    feat: Ui complete

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -22,16 +22,54 @@ const Login = () => {
                         </label>
                         <input type="password" placeholder="Password" className="w-full input input-bordered h-10" />
                     </div>
-                 <a href="" className="text-sm hover:underline hover:text-amber-200 mt-2 inline-block">
-                     Don't Have an account? 
-                 </a>
-                 <div>
-                 <button class="btn  btn-warning mt-2">Login</button>
-                 </div>
+                    <a href="" className="text-sm hover:underline hover:text-amber-200 mt-2 inline-block">
+                        Don't Have an account?
+                    </a>
+                    <div>
+                        <button class="btn  btn-warning mt-2">Login</button>
+                    </div>
                 </form>
             </div>
         </div>
     )
 }
 
+
+/* 
+const Login = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+            <div className="w-full p-6 rounded-lg shadow-md bg-gray-400">
+                <h1 className="text-3xl font-semibold text-center text-gray-300">
+                    Login
+                    <span className="text-yellow-400"> IChat</span>
+                </h1>
+
+                <form>
+                    <div>
+                        <label className="label p-3">
+                            <span className="text-amber-400 label-text">UserName</span>
+                        </label>
+                        <input type="text" placeholder="Enter UserName"
+                            className="w-full input input-bordered h-10" />
+                    </div>
+
+                    <div>
+                        <label className="label p-3">
+                            <span className="text-amber-400 label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="Password" className="w-full input input-bordered h-10" />
+                    </div>
+                    <a href="" className="text-sm hover:underline hover:text-amber-200 mt-2 inline-block">
+                        Don't Have an account?
+                    </a>
+                    <div>
+                        <button class="btn  btn-warning mt-2">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+*/
 export default Login;
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -45,7 +45,7 @@ const Signup = () => {
                             <input type="password" placeholder="Conform Password"
                                 className="w-full input input-bordered h-10" />
                         </div>
-                    {/* Checkbox  */}
+                    
                     <GenerateCheckBox></GenerateCheckBox>
                         <a href="" className="text-sm hover:underline hover:text-amber-200 mt-2 inline-block">
                         Already Have an Account?
@@ -61,4 +61,66 @@ const Signup = () => {
     )
 }
 
+/*
+const Signup = () => {
+    return (
+        <div>
+            <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+                <div className="w-full p-6 rounded-lg shadow-md bg-gray-400">
+                    <h1 className="text-3xl font-semibold text-center text-gray-200">
+                        Sign Up <span className="text-amber-400">IChat</span>
+                    </h1>
+                    <form >
+                        <div>
+                            <label className="label p-2">
+                                <span className="text-amber-400 label-text">
+                                    Full Name
+                                </span>
+                            </label>
+                            <input type="text" placeholder="Sajan Dost"
+                                className="w-full input input-bordered h-10" />
+                        </div>
+                        <div>
+                            <label className="label p-2">
+                                <span className="text-amber-400 label-text">
+                                    UserName
+                                </span>
+                            </label>
+                            <input type="text" placeholder="saujanKumarXXX"
+                                className="w-full input input-bordered h-10" />
+                        </div>
+                        <div>
+                            <label className="label p-2">
+                                <span className="text-amber-400 label-text">
+                                    Password
+                                </span>
+                            </label>
+                            <input type="password" placeholder="Password"
+                                className="w-full input input-bordered h-10" />
+                        </div>
+                        <div>
+                            <label className="label p-2">
+                                <span className="text-amber-400 label-text">
+                                    Conform Password
+                                </span>
+                            </label>
+                            <input type="password" placeholder="Conform Password"
+                                className="w-full input input-bordered h-10" />
+                        </div>
+                    
+                    <GenerateCheckBox></GenerateCheckBox>
+                        <a href="" className="text-sm hover:underline hover:text-amber-200 mt-2 inline-block">
+                        Already Have an Account?
+                    </a>
+
+                        <div>
+                            <button class="btn  btn-warning mt-2">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+*/
 export default Signup;
