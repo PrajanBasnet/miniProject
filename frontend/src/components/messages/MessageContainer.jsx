ed3fd928f196da7898b1d3e6b8b635b0a8f79c8a
prajwal <spiderbasnet140@gmail.com>
2025-07-21 09:39:25 +0545

    feat done

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -15,7 +15,7 @@ const MessageContainer = () =>{
         <div className='md:min-w-[450px] flex flex-col'>
           {!selectedConversation ? <NoChatSelected />:(
               <>
-              <div className='bg-slate-500 px-4 py-2 mb-2'>
+              <div className='bg-orange-500 px-4 py-2 mb-2'>
                       <span className="label-text">To:</span>{" "}
                       <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
                           </div>
